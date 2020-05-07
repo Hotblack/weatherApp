9a95f62c23c7dec482a5304a6eaa2913cc1b2b67
Adam Jackrel <ajackrel@gmail.com>
2020-05-07 10:07:03 -0400

    Initial Commit

---
+++ weatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by Adam Jackrel on 5/7/20.
+//  Copyright © 2020 Adam Jackrel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
